7058485f7fc74054f308b55fa70c2b4d034e1c16
kris876c <43539255+kris876c@users.noreply.github.com>
2019-02-27 11:12:21 +0100

    Add files via upload

---
+++ Application/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Application
+{
+    public class Controller
+    {
+        CustomerRepo customerRepo = new CustomerRepo();
+        public bool CheckCustomerID(int customerID)
+        {
+            if (customerRepo.GetCustomer(customerID) == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void CreateCustomer(int id, string name, string address, string zip, string town, string telephone)
+        {
+            Customer customer = new Customer(id, name, address, zip, town, telephone);
+            customerRepo.AddCustomer(customer);
+        }
+        public int GetCountOfCustomers()
+        {
+            return customerRepo.GetCountOfCustommers();
+        }
+        public string GetProducts()
+        {
+            return ProductRepo.
+        }
+    }
+}
---
+++ UI/NewCustomerWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Application;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for NewCustomerWindow.xaml
+    /// </summary>
+    public partial class NewCustomerWindow : Window
+    {
+        Controller control = new Controller();
+
+        public NewCustomerWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button_Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void CreateCustomer(object sender, RoutedEventArgs e)
+        {
+            int Id = control.GetCountOfCustomers() + 1;
+            control.CreateCustomer(Id, TextboxName.Text, TextboxAddress.Text, TextboxZIP.Text, TextboxCity.Text, TextboxPhone.Text);
+        }
+    }
+}
